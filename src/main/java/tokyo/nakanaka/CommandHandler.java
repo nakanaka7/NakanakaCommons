27c7eb2ee1f0f471942bd592e6f99c498bde1691
nakanakagames <nakanakagames@gmail.com>
2021-08-03 14:38:28 +0900

    fix

--- src/main/java/tokyo/nakanaka/CommandHandler.java
+++ src/main/java/tokyo/nakanaka/CommandHandler.java
@@ -1,11 +1,13 @@
 package tokyo.nakanaka;
 
+import java.util.List;
+
 import tokyo.nakanaka.commandSender.CommandSender;
 
 public interface CommandHandler {
 	
 	void onCommand(CommandSender cmdSender, String[] args);
 	
-	void onTabComplete(CommandSender cmdSender, String[] args);
+	List<String> onTabComplete(CommandSender cmdSender, String[] args);
 
 }
