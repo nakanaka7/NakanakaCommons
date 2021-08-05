b9411f641c07e48d24d54cc51f9f2203e33e0a68
nakanakagames <nakanakagames@gmail.com>
2021-08-05 11:26:22 +0900

    commit

--- src/main/java/tokyo/nakanaka/bukkit/BukkitScheduler.java
+++ src/main/java/tokyo/nakanaka/bukkit/BukkitScheduler.java
@@ -4,22 +4,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import tokyo.nakanaka.Scheduler;
-
+/**
+ * Scheduler for Bukkit
+ */
 public class BukkitScheduler implements Scheduler{
 	private JavaPlugin plugin;
-
+	/**
+	 * Constructs a scheduler
+	 * @param plugin a plugin which is responsible for a task
+	 */
 	public BukkitScheduler(JavaPlugin plugin) {
 		this.plugin = plugin;
 	}
 
 	@Override
-	public void scheduleLater(int tick, Runnable runnable) {
+	public void scheduleLater(int ticks, Runnable runnable) {
 		new BukkitRunnable() {
 			@Override
 			public void run() {
 				runnable.run();
 			}
-		}.runTaskLater(this.plugin, tick);
+		}.runTaskLater(this.plugin, ticks);
 	}
 	
 }
