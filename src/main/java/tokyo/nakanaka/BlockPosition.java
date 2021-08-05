a35d4eb41f97814b841f01a512407a3d42e0903d
nakanakagames <nakanakagames@gmail.com>
2021-08-05 09:15:16 +0900

    write javadoc comments

--- src/main/java/tokyo/nakanaka/BlockPosition.java
+++ src/main/java/tokyo/nakanaka/BlockPosition.java
@@ -1,4 +1,6 @@
 package tokyo.nakanaka;
-
+/**
+ * Represents a block position
+ */
 public record BlockPosition(World world, int x, int y, int z) {
 }
--- src/main/java/tokyo/nakanaka/CommandHandler.java
+++ src/main/java/tokyo/nakanaka/CommandHandler.java
@@ -3,11 +3,22 @@ package tokyo.nakanaka;
 import java.util.List;
 
 import tokyo.nakanaka.commandSender.CommandSender;
-
+/**
+ * Handles a command running and auto tab complete.
+ */
 public interface CommandHandler {
-	
+	/**
+	 * Invoked when the command is run
+	 * @param cmdSender a command sender who run the command
+	 * @param args arguments of the command
+	 */
 	void onCommand(CommandSender cmdSender, String[] args);
-	
+	/**
+	 * Returns a String list which will be used on auto tab complete for the command
+	 * @param cmdSender a command sender who run the command
+	 * @param args arguments of the command
+	 * @return a String list for the command
+	 */
 	List<String> onTabComplete(CommandSender cmdSender, String[] args);
 
 }
