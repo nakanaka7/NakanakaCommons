db0ab7c09d322f00cdede329cb490b681b91185e
nakanakagames <85822648+nakanakagames@users.noreply.github.com>
2021-08-05 11:59:54 +0900

    Delete doc directory

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"tokyo.nakanaka"},{"l":"tokyo.nakanaka.block"},{"l":"tokyo.nakanaka.bukkit"},{"l":"tokyo.nakanaka.bukkit.commandSender"},{"l":"tokyo.nakanaka.commandSender"},{"l":"tokyo.nakanaka.logger"},{"l":"tokyo.nakanaka.particle"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
