80084933b1fc1f909ed947ebba6275829517f557
nakanakagames <nakanakagames@gmail.com>
2021-08-05 11:19:46 +0900

    javadoc

--- src/main/java/tokyo/nakanaka/particle/Particle.java
+++ src/main/java/tokyo/nakanaka/particle/Particle.java
@@ -1,14 +1,22 @@
 package tokyo.nakanaka.particle;
 
 import tokyo.nakanaka.NamespacedID;
-
+/**
+ * Represents particle
+ */
 public class Particle {
 	private NamespacedID id;
-
+	/**
+	 * Construct particle
+	 * @param id namespacedID of the particle
+	 */
 	public Particle(NamespacedID id) {
 		this.id = id;
 	}
-
+	/**
+	 * Return the namespacedID of the particle
+	 * @return the namespacedID of the particle
+	 */
 	public NamespacedID getId() {
 		return id;
 	}
