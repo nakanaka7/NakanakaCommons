7a00515b90d4787a25b10037ac4bb5acfde53273
nakanakagames <nakanakagames@gmail.com>
2021-08-05 12:50:38 +0900

    javadoc

--- src/main/java/tokyo/nakanaka/commandSender/BlockCommandSender.java
+++ src/main/java/tokyo/nakanaka/commandSender/BlockCommandSender.java
@@ -1,5 +1,7 @@
 package tokyo.nakanaka.commandSender;
-
+/**
+ * Represents a command sender which is a command block
+ */
 public interface BlockCommandSender extends BlockPositionalCommandSender {
 	
 }
