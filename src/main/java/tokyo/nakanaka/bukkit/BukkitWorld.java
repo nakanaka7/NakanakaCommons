69a9aafb43e9d743fe87a2bf3376faf83b746df2
nakanakagames <nakanakagames@gmail.com>
2021-08-07 07:02:18 +0900

    implements getUniqueID method

--- src/main/java/tokyo/nakanaka/bukkit/BukkitWorld.java
+++ src/main/java/tokyo/nakanaka/bukkit/BukkitWorld.java
@@ -34,6 +34,11 @@ public class BukkitWorld implements World{
 	}
 	
 	@Override
+	public UUID getUniqueID() {
+		return this.uid;
+	}
+	
+	@Override
 	public Block getBlock(int x, int y, int z) {
 		org.bukkit.block.Block bukkitBlock = world.getBlockAt(x, y, z);
 		Block block = Block.valueOf(bukkitBlock.getBlockData().getAsString());
