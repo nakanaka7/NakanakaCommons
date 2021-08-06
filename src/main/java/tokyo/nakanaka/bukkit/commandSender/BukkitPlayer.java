595aefd3e7eaa7870a3dd4a94977ab665d742010
nakanakagames <nakanakagames@gmail.com>
2021-08-06 19:21:13 +0900

    implements getEntityPosition method

--- src/main/java/tokyo/nakanaka/bukkit/commandSender/BukkitPlayer.java
+++ src/main/java/tokyo/nakanaka/bukkit/commandSender/BukkitPlayer.java
@@ -3,6 +3,7 @@ package tokyo.nakanaka.bukkit.commandSender;
 import java.util.UUID;
 
 import tokyo.nakanaka.BlockPosition;
+import tokyo.nakanaka.EntityPosition;
 import tokyo.nakanaka.Player;
 import tokyo.nakanaka.World;
 import tokyo.nakanaka.bukkit.BukkitWorld;
@@ -32,6 +33,16 @@ public class BukkitPlayer extends BukkitCommandSender implements Player {
 	public UUID getUniqueID() {
 		org.bukkit.entity.Player player0 = (org.bukkit.entity.Player)this.sender;
 		return player0.getUniqueId();
+	}
+
+	@Override
+	public EntityPosition getEntityPosition() {
+		org.bukkit.entity.Player player0 = (org.bukkit.entity.Player)this.sender;
+		World world = new BukkitWorld(player0.getServer(), player0.getWorld());
+		double x = player0.getLocation().getX();
+		double y = player0.getLocation().getY();
+		double z = player0.getLocation().getZ();
+		return new EntityPosition(world, x, y, z);
 	}
 
 }
