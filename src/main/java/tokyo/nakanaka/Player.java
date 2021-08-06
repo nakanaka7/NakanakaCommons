680cc0012e1be4a87a00af6c4fa5f45b80e54ace
nakanakagames <nakanakagames@gmail.com>
2021-08-06 19:17:54 +0900

    add javadoc comments for getUniqueID method

--- src/main/java/tokyo/nakanaka/Player.java
+++ src/main/java/tokyo/nakanaka/Player.java
@@ -7,6 +7,10 @@ import tokyo.nakanaka.commandSender.BlockPositionalCommandSender;
  * Represents a player
  */
 public interface Player extends BlockPositionalCommandSender {
+	/**
+	 * Return the unique ID of the player 
+	 * @return the unique ID of the player
+	 */
 	UUID getUniqueID();
 	/**
 	 * Return the position of the player
