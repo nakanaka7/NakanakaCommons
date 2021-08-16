fa787e4c19e272348a78f641c20e5de7e1cbdc08
nakanakagames <nakanakagames@gmail.com>
2021-08-16 17:07:20 +0900

    enable to spawn some particles

--- src/main/java/tokyo/nakanaka/bukkit/BukkitWorld.java
+++ src/main/java/tokyo/nakanaka/bukkit/BukkitWorld.java
@@ -100,6 +100,8 @@ public class BukkitWorld implements World{
 		org.bukkit.Particle p;
 		Object data = null;
 		switch(name) {
+			//No use "FALLING_WATER", "ITEM_CRACK", "REVERSE_PORTAL", "SMALL_FLA<E",
+			//"SNOW_SHOVEL", "SUSPENDED_DEPTH"
 			case "ambient_entity_effect" -> p = org.bukkit.Particle.SPELL_MOB_AMBIENT;
 			case "angry_villager" -> p = org.bukkit.Particle.VILLAGER_ANGRY;
 			case "ash" -> p = org.bukkit.Particle.ASH;
@@ -115,7 +117,7 @@ public class BukkitWorld implements World{
 				}
 				data = this.server.createBlockData(bp.getBlock().getBlockStateString());
 			}
-			case "bubble" -> throw new IllegalArgumentException();
+			case "bubble" -> p = org.bukkit.Particle.WATER_BUBBLE;
 			case "bubble_column_up" -> p = org.bukkit.Particle.BUBBLE_COLUMN_UP;
 			case "bubble_pop" -> p = org.bukkit.Particle.BUBBLE_POP;
 			case "campfire_cosy_smoke" -> p = org.bukkit.Particle.CAMPFIRE_COSY_SMOKE;
@@ -124,7 +126,6 @@ public class BukkitWorld implements World{
 			case "composter" -> p = org.bukkit.Particle.COMPOSTER;
 			case "crimson_spore" -> p = org.bukkit.Particle.CRIMSON_SPORE;
 			case "crit" -> p = org.bukkit.Particle.CRIT;
-			//crit magic?
 			case "current_down" -> p = org.bukkit.Particle.CURRENT_DOWN;
 			case "damage_indicator" -> p = org.bukkit.Particle.DAMAGE_INDICATOR;
 			case "dolphin" -> p = org.bukkit.Particle.DOLPHIN;
@@ -146,14 +147,13 @@ public class BukkitWorld implements World{
 				data = new org.bukkit.Particle.DustOptions(Color.fromRGB(red, green, blue), dp.getSize());
 			}
 			case "dust_color_transition" -> p = org.bukkit.Particle.DUST_COLOR_TRANSITION;
-			case "effect" -> throw new IllegalArgumentException();
+			case "effect" -> p = org.bukkit.Particle.SPELL;
 			case "elder_guardian" -> p = org.bukkit.Particle.MOB_APPEARANCE;
 			case "electric_spark" -> p = org.bukkit.Particle.ELECTRIC_SPARK;
-			case "enchant" -> throw new IllegalArgumentException();
-			case "enchanted_hit" -> throw new IllegalArgumentException();
+			case "enchant" -> p = org.bukkit.Particle.ENCHANTMENT_TABLE;
+			case "enchanted_hit" -> p = org.bukkit.Particle.CRIT_MAGIC;
 			case "end_rod" -> p = org.bukkit.Particle.END_ROD;
-			case "entity_effect" -> throw new IllegalArgumentException();
-			//? -> EXPLOSION_NORMAL
+			case "entity_effect" -> p = org.bukkit.Particle.SPELL_MOB;
 			case "explosion" -> p = org.bukkit.Particle.EXPLOSION_LARGE;
 			case "explosion_emitter" -> p = org.bukkit.Particle.EXPLOSION_HUGE;
 			case "falling_dripstone_lava" -> p = org.bukkit.Particle.FALLING_DRIPSTONE_LAVA;
@@ -172,42 +172,43 @@ public class BukkitWorld implements World{
 			case "falling_spore_blossom" -> p = org.bukkit.Particle.FALLING_SPORE_BLOSSOM;
 			case "falling_water" -> p = org.bukkit.Particle.FALLING_WATER;
 			case "firework" -> p = org.bukkit.Particle.FIREWORKS_SPARK;
-			case "fishing" -> throw new IllegalArgumentException();
+			case "fishing" -> p = org.bukkit.Particle.WATER_WAKE;
 			case "flame" -> p = org.bukkit.Particle.FLAME;
 			case "flash" -> p = org.bukkit.Particle.FLASH;
 			case "glow" -> p = org.bukkit.Particle.GLOW;
 			case "glow_squid_ink" -> p = org.bukkit.Particle.GLOW_SQUID_INK;
 			case "happy_villager" -> p = org.bukkit.Particle.VILLAGER_HAPPY;
 			case "heart" -> p = org.bukkit.Particle.HEART;
-			case "instant_effect" -> throw new IllegalArgumentException();
+			case "instant_effect" -> p = org.bukkit.Particle.SPELL_INSTANT;
 			case "item" -> throw new IllegalArgumentException();
-			case "item_slime" -> throw new IllegalArgumentException();
-			case "item_snowball" -> throw new IllegalArgumentException();
+			case "item_slime" -> p = org.bukkit.Particle.SLIME;
+			case "item_snowball" -> p = org.bukkit.Particle.SNOWBALL;
 			case "landing_honey" -> p = org.bukkit.Particle.LANDING_HONEY;
 			case "landing_lava" -> p = org.bukkit.Particle.LANDING_LAVA;
 			case "landing_obsidian_tear" -> p = org.bukkit.Particle.LANDING_OBSIDIAN_TEAR;
-			case "large_smoke" -> throw new IllegalArgumentException();
+			case "large_smoke" -> p = org.bukkit.Particle.SMOKE_LARGE;
 			case "lava" -> p = org.bukkit.Particle.LAVA;
 			case "light" -> p = org.bukkit.Particle.LIGHT;
-			case "mycelium" -> throw new IllegalArgumentException();
+			case "mycelium" -> p = org.bukkit.Particle.TOWN_AURA;
 			case "nautilus" -> p = org.bukkit.Particle.NAUTILUS;
 			case "note" -> p = org.bukkit.Particle.NOTE;
-			case "poof" -> throw new IllegalArgumentException();
+			case "poof" -> p = org.bukkit.Particle.EXPLOSION_NORMAL;
 			case "portal" -> p = org.bukkit.Particle.PORTAL;
-			case "rain" -> throw new IllegalArgumentException();
+			case "rain" -> p = org.bukkit.Particle.WATER_DROP;
 			case "scrape" -> p = org.bukkit.Particle.SCRAPE;
-			case "smoke" -> throw new IllegalArgumentException();
+			case "smoke" -> p = org.bukkit.Particle.SMOKE_NORMAL;
 			case "sneeze" -> p = org.bukkit.Particle.SNEEZE;
-			case "snowflake" -> throw new IllegalArgumentException();
+			case "snowflake" -> p = org.bukkit.Particle.SNOWFLAKE;
 			case "soul" -> p = org.bukkit.Particle.SOUL;
 			case "soul_fire_flame" -> p = org.bukkit.Particle.SOUL_FIRE_FLAME;
 			case "spit" -> p = org.bukkit.Particle.SPIT;
 			case "spore_blossom_air" -> p = org.bukkit.Particle.SPORE_BLOSSOM_AIR;
-			case "splash" -> throw new IllegalArgumentException();
+			case "splash" -> p = org.bukkit.Particle.WATER_SPLASH;
 			case "squid_ink" -> p = org.bukkit.Particle.SQUID_INK;
 			case "sweep_attack" -> p = org.bukkit.Particle.SWEEP_ATTACK;
 			case "totem_of_undying" -> p = org.bukkit.Particle.TOTEM;
-			case "underwater" -> throw new IllegalArgumentException();
+			case "underwater" -> p = org.bukkit.Particle.SUSPENDED;
+			//SUSPENDED_DEPTH
 			case "vibration" -> p = org.bukkit.Particle.VIBRATION;
 			case "warped_spore" -> p = org.bukkit.Particle.WARPED_SPORE;
 			case "wax_off" -> p = org.bukkit.Particle.WAX_OFF;
