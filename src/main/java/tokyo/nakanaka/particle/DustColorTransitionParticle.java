542aa37b89304e772bce226c3a847c4180842b37
nakanakagames <nakanakagames@gmail.com>
2021-08-16 22:16:01 +0900

    modify argument/ method names

--- src/main/java/tokyo/nakanaka/particle/DustColorTransitionParticle.java
+++ src/main/java/tokyo/nakanaka/particle/DustColorTransitionParticle.java
@@ -5,49 +5,49 @@ import tokyo.nakanaka.NamespacedID;
  * Represents "minecraft:dust_color_transition" particle
  */
 public class DustColorTransitionParticle extends Particle {
-	private float redFrom;
-	private float greenFrom;
-	private float blueFrom;
+	private float fromRed;
+	private float fromGreen;
+	private float fromBlue;
 	private float size;
-	private float redTo;
-	private float greenTo;
-	private float blueTo;
+	private float toRed;
+	private float toGreen;
+	private float toBlue;
 	
 	/**
-	 * @param redFrom the initial red component of the color
-	 * @param greenFrom the initial green component of the color
-	 * @param blueFrom the initial blue component of the color
+	 * @param fromRed the initial red component of the color
+	 * @param fromGreen the initial green component of the color
+	 * @param fromBlue the initial blue component of the color
 	 * @param size the particle size
-	 * @param redTo the final red component of the color
-	 * @param greenTo the final green component of the color
-	 * @param blueTo the final blue component of the color
+	 * @param toRed the final red component of the color
+	 * @param toGreen the final green component of the color
+	 * @param toBlue the final blue component of the color
 	 */
-	public DustColorTransitionParticle(float redFrom, float greenFrom, float blueFrom, float size, float redTo, float greenTo, float blueTo) {
+	public DustColorTransitionParticle(float fromRed, float fromGreen, float fromBlue, float size, float toRed, float toGreen, float toBlue) {
 		super(new NamespacedID("minecraft", "dust_color_transition"));
-		this.redFrom = redFrom;
-		this.greenFrom = greenFrom;
-		this.blueFrom = blueFrom;
-		this.redTo = redTo;
-		this.greenTo = greenTo;
-		this.blueTo = blueTo;
+		this.fromRed = fromRed;
+		this.fromGreen = fromGreen;
+		this.fromBlue = fromBlue;
+		this.toRed = toRed;
+		this.toGreen = toGreen;
+		this.toBlue = toBlue;
 	}
 	/**
 	 * @return an initial red component
 	 */
-	public float getRedFrom() {
-		return redFrom;
+	public float getFromRed() {
+		return fromRed;
 	}
 	/**
 	 * @return an initial green component
 	 */
-	public float getGreenFrom() {
-		return greenFrom;
+	public float getFromGreen() {
+		return fromGreen;
 	}
 	/**
 	 * @return an initial blue component
 	 */
-	public float getBlueFrom() {
-		return blueFrom;
+	public float getFromBlue() {
+		return fromBlue;
 	}
 
 	public float getSize() {
@@ -56,20 +56,20 @@ public class DustColorTransitionParticle extends Particle {
 	/**
 	 * @return a final red component
 	 */
-	public float getRedTo() {
-		return redTo;
+	public float getToRed() {
+		return toRed;
 	}
 	/**
 	 * @return a final green component
 	 */
-	public float getGreenTo() {
-		return greenTo;
+	public float getToGreen() {
+		return toGreen;
 	}
 	/**
 	 * @return a final blue component
 	 */
-	public float getBlueTo() {
-		return blueTo;
+	public float getToBlue() {
+		return toBlue;
 	}
 	
 }
